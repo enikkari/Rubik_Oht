1305c5aa9aff0f69f9308769cdf0ca2172841537
Eeva <eevanikkari@hotmail.fi>
2013-04-01 18:23:05 +0300

    --

--- Rubik_Oht/src/rubik_oht/Rubik_Oht.java
+++ Rubik_Oht/src/rubik_oht/Rubik_Oht.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 public class Rubik_Oht {
 
     public static void main(String[] args) {
-        // TODO code application logic here
        
          RubikinKuutio jeejee = new RubikinKuutio("y","v","e","o","a","t");
         System.out.println(jeejee.tulostaKuutio());
